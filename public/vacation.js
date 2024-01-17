538c5f5b15bf8954245dbea50259ba938fd7cfc2
AP500 <and.perschin@gmail.com>
2024-01-17 12:50:35 +0700

    date

--- public/vacation.js
+++ public/vacation.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $.get('/vacation', function(data) {
         data.forEach(function(vacation) {
-            const startDate = new Date(vacation.beginning).toISOString().substring(0, 10);
-            const endDate = new Date(vacation.ending).toISOString().substring(0, 10);
+            const startDate = vacation.beginning.substring(0, 10);
+            const endDate = vacation.ending.substring(0, 10);
 
             const vacationElement = $('<div>').text(
                 `Name: ${vacation.name},  Reason: ${vacation.reasonL},  Start Date: ${startDate},  End Date: ${endDate}`
